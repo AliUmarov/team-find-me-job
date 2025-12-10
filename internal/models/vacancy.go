1a4d65649d0070a706b4bd30001dbd2156c32f50
fw <dasler-fw@inbox.ru>
2025-12-10 12:20:14 +0300

    add json tags to the resume

--- internal/models/vacancy.go
+++ internal/models/vacancy.go
@@ -3,17 +3,19 @@ package models
 type Vacancy struct {
 	Base
 
-	Title       string
-	Description string
-	Salary      int
-	Rating      float64
-	CompanyID   uint
+	Title       string  `json:"title" gorm:"type:varchar(255);not null"`
+	Description string  `json:"description" gorm:"type:text;not null"`
+	Salary      int     `json:"salary" gorm:"type:int;not null"`
+	Rating      float64 `json:"rating" gorm:"type:float;not null"`
+	CompanyID   uint    `json:"company_id" gorm:"not null"`
+
+	Company *Company `json:"-" gorm:"foreignKey:CompanyID"`
 }
 
 type CreateVacancy struct {
-	Title       string
-	Description string
-	Salary      int
-	Rating      float64
-	CompanyID   uint
+	Title       string  `json:"title"`
+	Description string  `json:"description"`
+	Salary      int     `json:"salary"`
+	Rating      float64 `json:"rating"`
+	CompanyID   uint    `json:"company_id"`
 }
