a38a712071d96382f5d8ef35947c82b541b971aa
Ali Umarov <aliumarov90@mail.ru>
2025-12-11 18:52:09 +0300

    вынес в константы ошибки из resume_handler

---
+++ internal/constants/resume.go
@@ -0,0 +1,10 @@
+package constants
+
+const (
+	ERR_CAN_NOT_CREATE_RESUME = "cannot create resume"
+	ERR_CAN_NOT_GET_RESUME    = "cannot get resume"
+	ERR_CAN_NOT_UPDATE_RESUME = "cannot update resume"
+	ERR_CAN_NOT_DELETE_RESUME = "cannot delete resume"
+	ERR_INVALID_JSON          = "invalid JSON"
+	ERR_INVALID_ID            = "invalid id"
+)
